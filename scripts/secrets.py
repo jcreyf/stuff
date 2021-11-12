1699abf3556d4bff80a23a5387c802c585a67cfc
Creyf, Jo <jo.creyf@gmail.com>
2021-11-11 23:03:13 -0800

    This version seems to be working good

--- scripts/secrets.py
+++ scripts/secrets.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 #
-# This Crypto implementation:
+# We're using this Crypto implementation for the AES 256bit CBC cipher logic:
 #   pip install pycryptodome
 #   https://stackoverflow.com/questions/12524994/encrypt-decrypt-using-pycrypto-aes-256
 #
@@ -17,15 +17,54 @@ class AES_256_CBC_Cipher(object):
     Initial version cloned from:
       https://stackoverflow.com/questions/12524994/encrypt-decrypt-using-pycrypto-aes-256
   """
-  def __init__(self, key):
-    self.bs = AES.block_size
-    self.key = hashlib.sha256(key.encode()).digest()
 
-  def encrypt(self, raw):
+  def __init__(self, key):
+    """
+      Constructor, setting the encryption key.
+      If we have a secondary "special key" on the system, then that secondary key
+      will be used to encrypt the encryption key.  That encrypted key then becomes the
+      actual key to encrypt and decrypt secrets.
+    """
+    self.bs=AES.block_size
+    # Set the secondary optional "special key" (if we have one):
+    try:
+      with open("key.txt","r") as f:
+        _specialKey=f.readline()
+      f.close()
+      # Remove potential newline characters from the string:
+      _specialKey=_specialKey.replace("\n", "")
+#      print(f"Special: '{_specialKey}'")
+    except:
+      # Ignore any and all exceptions to truly make the "special key" optional.
+      _specialKey=""
+
+    if _specialKey == "":
+      # Use the given key if we don't have a "special key":
+      self.key = hashlib.sha256(key.encode()).digest()
+    else:
+      # Aha!  We have a secondary key to make things "special".
+      self.key=hashlib.sha256(_specialKey.encode()).digest()
+#      print(f"special key: {self.key}")
+      _bytes=self.encrypt(key, special=True)
+#      print(f"encrypted: {_bytes}")
+      self.key=hashlib.sha256(_bytes).digest()
+
+
+  def encrypt(self, raw, special=False):
     raw = self._pad(raw)
-    iv = Random.new().read(AES.block_size)
+    # This method is also called to encrypt the "special key" (optional secondary key).
+    # That optional key needs to get encrypted with the same exact seed every single
+    # time to make sure it doesn't change.
+    # Secrets encrypted with both the key and secondary key will no longer be decryptable
+    # if not the exact same key is used that was used to encrypt the extra key.
+    if special:
+      iv = b"\xf4\r\xb7\x1b\xbb7\x0e`CX&\x0c\xf7O1\x08"
+    else:
+      iv = Random.new().read(AES.block_size)
+
     cipher = AES.new(self.key, AES.MODE_CBC, iv)
     return base64.b64encode(iv + cipher.encrypt(raw.encode()))
+
 
   def decrypt(self, enc):
     enc = base64.b64decode(enc)
@@ -33,24 +72,26 @@ class AES_256_CBC_Cipher(object):
     cipher = AES.new(self.key, AES.MODE_CBC, iv)
     return self._unpad(cipher.decrypt(enc[AES.block_size:])).decode('utf-8')
 
+
   def _pad(self, s):
     return s + (self.bs - len(s) % self.bs) * chr(self.bs - len(s) % self.bs)
+
 
   @staticmethod
   def _unpad(s):
     return s[:-ord(s[len(s)-1:])]
 
-
-VERBOSE=False
-def verbose(str):
-  if VERBOSE:
-    print(str)
-
+# ------
 
 if __name__ == "__main__":
+  VERBOSE=False
+  def verbose(str):
+    if VERBOSE:
+      print(str)
+
   # Define the command-line arguments that the app supports:
   parser=argparse.ArgumentParser(description="Encrypt or Decrypt secrets.")
-  parser.add_argument("-?", \
+  parser.add_argument("--version", \
                         action="version", \
                         version="%(prog)s 1.0")
   parser.add_argument("-v", "--verbose", \
@@ -97,20 +138,6 @@ if __name__ == "__main__":
   key=__ARGS.__KEY
   pwd=__ARGS.__PWD
   ENCRYPT=__ARGS.__ENCRYPT
-
-###
-  _specialKey="SzhkTDdQYVpzaCt0SitIaVdwNDIzTHlHamEwdGlFakdOSFp2alZzUVpOaz0="
-  enc=AES_256_CBC_Cipher(_specialKey)
-  try:
-    encode_bytes=key.encode("utf-8")
-    base_bytes=base64.b64decode(encode_bytes)
-    base_txt=base_bytes.decode("utf-8")
-    secret=base_txt
-    verbose(secret)
-    print(enc.decrypt(secret))
-  except BaseException as err:
-    sys.exit(f"Bad key!")
-###
 
   enc=AES_256_CBC_Cipher(key)
 
