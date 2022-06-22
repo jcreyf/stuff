dd99332eadbbcf1c4b3bcb72b0a5f40ea2bbc522
Jo Creyf <jo.creyf@gmail.com>
2022-06-21 17:28:52 -0700

    Change the default delimiter to deal with spaces

--- deleteDSStore/deleteDSStore.sh
+++ deleteDSStore/deleteDSStore.sh
@@ -21,6 +21,10 @@ log()
 
 #log "running: deleteDSStore.sh"
 
+# Change the default space delimiter to deal with directories that may have a space in their name:
+_IFS=$IFS
+IFS=$'\n'
+# Find all the .DS_Store files in the home directory:
 _files=($(find ~ -name ".DS_Store" 2>/dev/null))
 log "${#_files[@]} '.DS_Store' files to delete:"
 for _file in ${_files[*]}
@@ -28,6 +32,8 @@ do
   log "  $_file"
   rm -f $_file
 done
+# Restore the default delimiter:
+IFS=$_IFS
 
 # The find command will exit with "1" if no files found.
 # Exit the script with a success code to avoid unnecessary launchd messages logging in /var/log/system.log
