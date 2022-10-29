37e10ad42f67a921b0de5ea262202d7c6fa72f8d
Creyf, Jo <jo.creyf@gmail.com>
2022-10-28 23:24:10 -0700

    Extend schema validation

---
+++ slack_stay_active/config_check.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+# ------------------------------------------------------
+# YAML schema validation:
+#     * https://docs.python-cerberus.org/en/stable/  
+#
+# /> conda install -c conda-forge cerberus
+#      The following packages will be downloaded:
+#        package                    |            build
+#        ---------------------------|-----------------
+#        ca-certificates-2022.10.11 |       h06a4308_0         124 KB
+#        cerberus-1.3.4             |     pyhd8ed1ab_0          51 KB  conda-forge
+#        certifi-2022.9.24          |     pyhd8ed1ab_0         155 KB  conda-forge
+#        openssl-1.1.1q             |       h7f8727e_0         2.5 MB
+#        ------------------------------------------------------------
+#        Total:         2.8 MB
+# ------------------------------------------------------
+
+import yaml
+from cerberus import Validator
+
+_configFile = './slack_active.yaml'
+_schemaFile = './config.schema'
+
+def load_doc():
+    with open(_configFile, 'r') as stream:
+        try:
+            return yaml.safe_load(stream)
+        except yaml.YAMLError as exception:
+            raise exception
+
+
+## Now, validating the yaml file is straightforward:
+schema = eval(open(_schemaFile, 'r').read())
+v = Validator(schema)
+doc = load_doc()
+print(v.validate(doc))
+print(v.errors)
