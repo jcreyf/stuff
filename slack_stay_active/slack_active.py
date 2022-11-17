bd69f6cdf8427a6cfe89c14c59294d05f74eedc9
Jo Creyf <jo.creyf@gmail.com>
2022-11-16 19:21:56 -0800

    Adding RPi ChromeDriver code

--- slack_stay_active/slack_active.py
+++ slack_stay_active/slack_active.py
@@ -623,7 +623,7 @@ class SlackActive:
         # (we could also parse the data out of "os.uname()")
         if os.name == 'posix':
             self.log("Running on a Raspberry PI...")
-            _chrome_version = Service(ChromeDriverManager())
+            _chrome_version = Service('/usr/bin/chromedriver')
         else:
             # Run this on non-RPi devices:
             _chrome_service = Service(ChromeDriverManager(version=_chrome_version).install())
